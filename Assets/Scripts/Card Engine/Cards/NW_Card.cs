5850a9805a2fc6d8f5638a33264413e18c1c25c4
Barnaff <kobi.chariski@gmail.com>
2015-04-12 10:01:27 +0300

    Added cards editor

---
+++ Assets/Scripts/Card Engine/Cards/NW_Card.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System;
+
+[Flags]
+public enum NW_CardType
+{
+	Creature,
+	Spell,
+	Legandary,
+	Artifact,
+}
+
+[Flags]
+public enum NW_Color
+{
+	Colorless,
+	Black,
+	Blue,
+	White,
+	Green,
+	Red,
+	Purple,
+}
+
+[XmlRoot("Card")]
+public class NW_Card  {
+
+	[XmlElement("CardName")]
+	public string CardName;
+	
+	[XmlElement("CardId")]
+	public string CardId = "Empty";
+	
+	[XmlElement("CastingCost")]
+	public int CastingCost;
+	
+	[XmlElement("Thrashold")]
+	public string Thrashold;
+	
+	[XmlElement("ImageName")]
+	public string ImageName;
+	
+	[XmlElement("InfoText")]
+	public string InfoText;
+
+	[XmlArray("CardTypes")]
+	[XmlArrayItem("CardType")]
+	public List<NW_CardType> CardTypes;
+
+	[XmlArray("CardColors")]
+	[XmlArrayItem("CardColor")]
+	public List<NW_Color> CardColors;
+
+	[XmlElement("Power")]
+	public int Power;
+	
+	[XmlElement("Toughness")]
+	public int Toughness;
+
+
+
+}
---
+++ Assets/Scripts/Card Engine/Cards/NW_CardsLoader.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public class NW_CardsLoader  {
+	
+	public static string CARDS_RESOURCE_PATH = "/Resources/Cards/";
+	public static string CARDS_RESOURCE_EXTENSION = ".xml";
+
+	public static string[] GetAllCardsFilesPaths()
+	{
+		string dir = Application.dataPath;
+		string[] filePaths = Directory.GetFiles(dir + NW_CardsLoader.CARDS_RESOURCE_PATH, "*" + NW_CardsLoader.CARDS_RESOURCE_EXTENSION);
+		return filePaths; 
+	}
+	
+	public static List<NW_Card> LoadAllCards()
+	{
+		string[] cardsFilesPaths = GetAllCardsFilesPaths();
+		List <NW_Card> cards = new List<NW_Card>();
+		foreach (string cardFilePath in cardsFilesPaths)
+		{
+			//Debug.Log("cardFilePath: " + cardFilePath);
+			NW_Card card = NW_CardsLoader.LoadCardFile(cardFilePath);
+			if (card != null)
+			{
+				cards.Add(card);
+			}
+		}
+		return cards;
+	}
+	
+	
+	public static void SaveCardToFile(NW_Card card)
+	{
+		string dir = Application.dataPath;
+		string filePath = Path.Combine(dir + NW_CardsLoader.CARDS_RESOURCE_PATH, card.CardId + NW_CardsLoader.CARDS_RESOURCE_EXTENSION);
+		Debug.Log("save card: " + filePath);
+		var serializer = new XmlSerializer(typeof(NW_Card));
+		using(var stream = new FileStream(filePath, FileMode.Create))
+		{
+			serializer.Serialize(stream, card);
+			stream.Close();
+		}
+	}
+	
+	public static NW_Card LoadCardFile(string filePath)
+	{
+		var serializer = new XmlSerializer(typeof(NW_Card));
+		var stream = new FileStream(filePath, FileMode.Open);
+		NW_Card card = serializer.Deserialize(stream) as NW_Card;
+		stream.Close();
+		return card;
+	}
+	
+	public static NW_Card LoadCard(string cardId)
+	{
+		string dir = Application.dataPath;
+		string filePath = Path.Combine(dir + NW_CardsLoader.CARDS_RESOURCE_PATH, cardId + NW_CardsLoader.CARDS_RESOURCE_EXTENSION);
+		return NW_CardsLoader.LoadCardFile(filePath);
+	}
+}
