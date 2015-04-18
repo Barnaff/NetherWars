5f73cef6a770e7166aaf408d6814e141fd8b5ed2
shugu <eyatziv@gmail.com>
2015-04-18 15:36:36 +0300

    After crash commit
    remade lost scene and readded template scripts (empty versions)

---
+++ Assets/Scripts/Gameplay/Controllers/Zones/ZoneControllerAbstract.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneControllerAbstract : MonoBehaviour
+{
+	private NW_Zone m_zone;
+	[SerializeField]
+	private ZoneType m_zoneType;
+
+	public void InitWithZone(NW_Zone i_Zone)
+	{
+		this.m_zone = i_Zone;
+	}
+}
